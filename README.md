00a7b3e973cc12e0f8c18583cdc74ad4cde1066a
0xKubitus <0xKubitus@gmail.com>
2023-03-12 22:12:03 +0100

    step2 in progress

--- README.md
+++ README.md
@@ -9,5 +9,27 @@
 </ol>
 
 <small>
-<em>/!\ The format of the modified .csv file (its headers) should be generic so that I can use it as a template to modify other CryptoExchanges' transactions history .csv files...</em>
+<em>/!\ The format of the modified .csv file (its headers) should be generic so that it can be used later to group other CryptoExchanges' transactions history .csv files...</em>
 </small>
+
+<br>
+<br>
+<hr>
+<br>
+
+## How to use this tool?
+
+<ol>
+<li>Create a new folder.</li>
+<li>Clone this repo inside your new folder, and add your Binance 'Transaction History' .csv file inside this folder.</li>
+<li>Open "binance-CSV-editor.rb" in your IDE and modify: transactions_set_to_be_modified = 'your-Binance-Transaction-History-file.csv' according to the name of <em>'your-Binance-Transaction-History-file'</em>.</li>
+<li>
+  Open the folder in a Terminal window and execute 'binance-CSV-editor.rb':
+  <br>
+  <code>ruby binance-CSV-editor.rb</code>
+</li>
+<li></li>
+<li></li>
+<li></li>
+</ol>
+```
--- binance-CSV-editor.rb
+++ binance-CSV-editor.rb
@@ -7,7 +7,8 @@ GOAL = EACH TRANSACTION SHOULD:
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-STEP 1: Set appropriate headers to the new .csv file
+STEP 1: Set appropriate headers to the new .csv file = DONE
+STEP 2 => Make sure that each transaction's details are  going into the right column of the new .csv file = IN PROGRESS
 
 
 =end
@@ -17,18 +18,87 @@ STEP 1: Set appropriate headers to the new .csv file
 
 require 'csv'
 
-# Assign the original binance file to a variable
-transactions_set_to_be_modified = 'binance_transactions-year_2021.csv'
+# Assign the original binance file to a variable:
+transactions_set_to_be_modified = 'binance_transactions-year_2021.csv' # <= /!\ REPLACE 'binance_transactions-year_2021.csv' WITH THE NAME OF YOUR BINANCE TRANSACTION HISTORY FILE /!\ 
 
-# Read the transactions set to be modified
+# Read the transactions set to be modified:
 transactions = CSV.read(transactions_set_to_be_modified)
 
-# Create a new CSV file named 'modified_binance_transactions_set.csv'
+# Create a new CSV file named 'modified_binance_transactions_set.csv':
 CSV.open('modified_binance_transactions_set.csv', 'w') do |csv|
   
-  # Write the headers for the modified transactions
-  csv << ['Platform: Exchange / Wallet', 'UTC_Time', 'Account / Public Address', 'Operation', 'Credited Asset', 'Credited Amount', 'Debited Asset', 'Debited Amount', 'Fee Asset', 'Fee Amount', 'Remark/Comments']
+  # Write the headers for the modified transactions:
+  csv << ['Platform: Exchange / Wallet', 'UTC_Time', 'Account / Public Address', 'Operation', 'Credited Asset', 'Credited Amount', 'Debited Asset', 'Debited Amount', 'Fee Asset', 'Fee Amount', 'Remark/Comments', 'Tx Id']
   
+  # Loop through each transaction in the original CSV file:
+  transactions.each_with_index do |transaction, index|
+  
+    # Skip the first row, which contains the headers:
+    next if index == 0
+    
+    
+    # Assign the transaction details to variables:
+    platform = 'Binance' # (this value will replace the "User_Id" column from the original CSV file)
+    timestamp = transaction[1]
+    account = transaction[2]
+    operation = transaction[3]
+    coin = transaction[4]
+    change = transaction[5]
+    remark = transaction[6]
+
+    # Define the credited asset & amount, the debited asset & amount, and eventual fee asset & amount for the transaction, depending of its type of 'Operation'
+    case operation
+
+    # OPERATION TYPE = "RECEIVE"
+    when 'Deposit', 'Savings Distribution', 'BNB Vault Rewards'
+      credited_asset = coin
+      credited_amount = change
+      debited_asset = ''
+      debited_amount = ''
+      fee_asset = ''
+      fee_amount = ''
+     
+
+    # OPERATION TYPE = "SEND"
+    when 'Withdrawal' 
+      credited_asset = ''
+      credited_amount = ''
+      debited_asset = coin
+      debited_amount = change
+      fee_asset = ''
+      fee_amount = ''
+    
+
+    # SPECIAL CASE: OPERATION TYPE = "TRADE"
+    when 'Binance Convert'  
+      if change > 0
+        credited_asset = coin
+        credited_amount = change
+        debited_asset = ''
+        debited_amount = ''
+        fee_asset = ''
+        fee_amount = ''
+      elsif change < 0
+        credited_asset = ''
+        credited_amount = ''
+        debited_asset = coin
+        debited_amount = change
+        fee_asset = ''
+        fee_amount = ''
+      end
+     
+
+    when 'Fee' # OPERATION TYPE = "FEES"
+      credited_asset = ''
+      credited_amount = ''
+      debited_asset = ''
+      debited_amount = ''
+      fee_asset = coin
+      fee_amount = change
+    end
+
+  end
+
 end
 
 
@@ -38,7 +108,6 @@ end
 
 
 NEXT STEPS:
-STEP 2 => Make sure that each transaction's data goes into the right column of the new .csv file;
 STEP 3 => Iterate through each transaction to find related transactions and compile them into a single line.
 
 
