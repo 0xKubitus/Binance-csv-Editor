c6b4ebcfad9c372349d12d8a40a6efd2d14fd6ed
0xKubitus <0xKubitus@gmail.com>
2023-03-16 14:08:55 +0100

    minor update

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ The format of the modified .csv file (its headers) should be generic so that it 
    -> your binance file should look like this: "part-00000-8aae60e2-9a76-4765-98b2-9d4f5ff6eff8-c000.csv"
 3. Open "binance-CSV-editor.rb" in your IDE and modify: transactions_set_to_be_modified = 'your-Binance-Transaction-History-file.csv' according to the name of <em>'your-Binance-Transaction-History-file'</em>.
 4. Open the folder in a Terminal window,and run the following command:<br><code>$ ruby binance-CSV-editor.rb</code>
-5. Check your folder: it now includes 'modified_binance_transactions_set.csv' with all your Binance transactions, each taking a single row of the table.
+5. Check your folder: it now includes 'all_binance_transactions_grouped-together.csv' with all your Binance transactions, each taking a single row of the table.
 6. Now you can do the same with your other crypto-exchanges transaction history .csv files and regroup everything using this template!
 7. I will put here the links to my similar projects for Coinbase and Crypto.com/exchange...
 </ol>
--- binance-CSV-editor.rb
+++ binance-CSV-editor.rb
@@ -18,138 +18,145 @@ STEP 2 => Make sure that each transaction's details are  going into the right co
 
 require 'csv'
 
-# Assign the original binance file to a variable:
-transactions_set_to_be_modified = 'my-binance-transactions-up-to-March-12-2023.csv' # <= /!\ REPLACE 'binance_transactions-year_2021.csv' WITH THE NAME OF YOUR BINANCE TRANSACTION HISTORY FILE /!\ 
-# Read the transactions set to be modified:
-transactions = CSV.read(transactions_set_to_be_modified)
-# Create a new CSV file named 'modified_binance_transactions_set.csv':
-CSV.open('modified_binance_transactions_set.csv', 'w') do |csv|
-  # Write the headers for the modified transactions:
-  csv << ['Platform: Exchange / Wallet', 'UTC_Time', 'Account / Public Address', 'Operation', 'Credited Asset', 'Credited Amount', 'Debited Asset', 'Debited Amount', 'Fee Asset', 'Fee Amount', 'Remark/Comments', 'Tx Id']
-  
-  # Loop through each transaction in the original CSV file:
-  transactions.each_with_index do |transaction, index|
-    # Skip the first row, which contains the headers:
-    next if index == 0
+
+def group_all_csv_files
+
+  # Assign the original binance file to a variable:
+  transactions_set_to_be_modified = 'my-binance-transactions-up-to-March-12-2023.csv' # <= /!\ REPLACE 'binance_transactions-year_2021.csv' WITH THE NAME OF YOUR BINANCE TRANSACTION HISTORY FILE /!\
+
+  # Read the transactions set to be modified:
+  transactions = CSV.read(transactions_set_to_be_modified)
+
+  # Create a new CSV file named 'all_binance_transactions_grouped-together.csv':
+  CSV.open('all_binance_transactions_grouped-together.csv', 'w') do |csv|
     
-    # Assign the transaction details to variables:
-    platform = 'Binance' # (this value will replace the "User_Id" column from the original CSV file)
-    timestamp = transaction[1]
-    account = transaction[2]
-    operation = transaction[3]
-    coin = transaction[4]
-    change = transaction[5].to_f
-    remark = transaction[6]
+    # Write the headers for the modified transactions:
+    csv << ['Platform: Exchange / Wallet', 'UTC_Time', 'Account / Public Address', 'Operation', 'Credited Asset', 'Credited Amount', 'Debited Asset', 'Debited Amount', 'Fee Asset', 'Fee Amount', 'Remark/Comments', 'Tx Id']
+    
+    # Loop through each transaction in the original CSV file:
+    transactions.each_with_index do |transaction, index|
+      # Skip the first row, which contains the headers:
+      next if index == 0
+      
+      # Assign the transaction details to variables:
+      platform = 'Binance' # (this value will replace the "User_Id" column from the original CSV file)
+      timestamp = transaction[1]
+      account = transaction[2]
+      operation = transaction[3]
+      coin = transaction[4]
+      change = transaction[5].to_f
+      remark = transaction[6]
 
-    puts "transaction[5].class = " # SHOULD BE A COMMENT
-    puts transaction[5].class # SHOULD BE A COMMENT
-    # puts "change.class = " # SHOULD BE A COMMENT
-    # puts change.class # SHOULD BE A COMMENT
-    # puts "- - - - -" # SHOULD BE A COMMENT
+      puts "transaction[5].class = " # SHOULD BE A COMMENT
+      puts transaction[5].class # SHOULD BE A COMMENT
+      # puts "change.class = " # SHOULD BE A COMMENT
+      # puts change.class # SHOULD BE A COMMENT
+      # puts "- - - - -" # SHOULD BE A COMMENT
 
 
-    # Define the credited asset & amount, the debited asset & amount, 
-    # and eventual fee asset & amount for the transaction, depending of its type of 'Operation'
-    case operation
+      # Define the credited asset & amount, the debited asset & amount, 
+      # and eventual fee asset & amount for the transaction, depending of its type of 'Operation'
+      case operation
 
-    # OPERATION TYPE = "RECEIVE"
-    when 'Deposit', 'Savings Distribution', 'BNB Vault Rewards', 'Distribution', 'ETH 2.0 Staking Rewards', 'Savings Distribution', 'Simple Earn Flexible Airdrop', 'Simple Earn Flexible Interest', 'Simple Earn Locked Rewards', 'Staking Redemption', 'Staking Rewards', 'Transaction Buy', 'Transaction Revenue'
-      credited_asset = coin
-      credited_amount = change
-      debited_asset = ''
-      debited_amount = ''
-      fee_asset = ''
-      fee_amount = ''
-
-    # OPERATION TYPE = "SEND"
-    when 'Withdraw', 'Simple Earn Flexible Subscription', 'Simple Earn Locked Subscription', 'Staking Purchase', 'Transaction Related', 'Transaction Sold', 'Transaction Spend'
-      credited_asset = ''
-      credited_amount = ''
-      debited_asset = coin
-      debited_amount = change
-      fee_asset = ''
-      fee_amount = ''
-
-    # SPECIAL CASES - OPERATION TYPE = "SEND or RECEIVE" 
-    # despite occuring at the same time, both transactions must remain separate as these are not trades!
-    when 'ETH 2.0 Staking', 'Simple Earn Flexible Redemption' 
-      if change > 0
+      # OPERATION TYPE = "RECEIVE"
+      when 'Deposit', 'Savings Distribution', 'BNB Vault Rewards', 'Distribution', 'ETH 2.0 Staking Rewards', 'Savings Distribution', 'Simple Earn Flexible Airdrop', 'Simple Earn Flexible Interest', 'Simple Earn Locked Rewards', 'Staking Redemption', 'Staking Rewards', 'Transaction Buy', 'Transaction Revenue'
         credited_asset = coin
         credited_amount = change
         debited_asset = ''
         debited_amount = ''
         fee_asset = ''
         fee_amount = ''
-      elsif change < 0
+
+      # OPERATION TYPE = "SEND"
+      when 'Withdraw', 'Simple Earn Flexible Subscription', 'Simple Earn Locked Subscription', 'Staking Purchase', 'Transaction Related', 'Transaction Sold', 'Transaction Spend'
         credited_asset = ''
         credited_amount = ''
         debited_asset = coin
         debited_amount = change
         fee_asset = ''
         fee_amount = ''
-      end
 
-    # SPECIAL CASES - OPERATION TYPE = "TRADE"
-    when 'Binance Convert', 'Small Assets Exchange BNB', 'Stablecoins Auto-Conversion'
-      if change > 0
-        credited_asset = coin
-        credited_amount = change
-        debited_asset = ''
-        debited_amount = ''
-        fee_asset = ''
-        fee_amount = ''
-      elsif change < 0
+      # SPECIAL CASES - OPERATION TYPE = "SEND or RECEIVE" 
+      # despite occuring at the same time, both transactions must remain separate as these are not trades!
+      when 'ETH 2.0 Staking', 'Simple Earn Flexible Redemption' 
+        if change > 0
+          credited_asset = coin
+          credited_amount = change
+          debited_asset = ''
+          debited_amount = ''
+          fee_asset = ''
+          fee_amount = ''
+        elsif change < 0
+          credited_asset = ''
+          credited_amount = ''
+          debited_asset = coin
+          debited_amount = change
+          fee_asset = ''
+          fee_amount = ''
+        end
+
+      # SPECIAL CASES - OPERATION TYPE = "TRADE"
+      when 'Binance Convert', 'Small Assets Exchange BNB', 'Stablecoins Auto-Conversion'
+        if change > 0
+          credited_asset = coin
+          credited_amount = change
+          debited_asset = ''
+          debited_amount = ''
+          fee_asset = ''
+          fee_amount = ''
+        elsif change < 0
+          credited_asset = ''
+          credited_amount = ''
+          debited_asset = coin
+          debited_amount = change
+          fee_asset = ''
+          fee_amount = ''
+        end
+      when 'Buy Crypto'
+        if change > 0 && coin == 'EUR' # <= transaction = FIAT (EUR in my case) purchase and deposit on account 
+          credited_asset = coin
+          credited_amount = change
+          debited_asset = ''
+          debited_amount = ''
+          fee_asset = ''
+          fee_amount = ''
+        elsif change < 0 # <= transaction = debit (should be FIAT - EUR in my case)
+          credited_asset = ''
+          credited_amount = ''
+          debited_asset = coin
+          debited_amount = change
+          fee_asset = ''
+          fee_amount = ''
+        elsif change > 0 && coin != 'EUR' # <= transaction = credit (crypto)
+          credited_asset = coin
+          credited_amount = change
+          debited_asset = ''
+          debited_amount = ''
+          fee_asset = ''
+          fee_amount = ''
+        end
+
+      when 'Fee' # OPERATION TYPE = "FEES"
         credited_asset = ''
         credited_amount = ''
-        debited_asset = coin
-        debited_amount = change
-        fee_asset = ''
-        fee_amount = ''
-      end
-    when 'Buy Crypto'
-      if change > 0 && coin == 'EUR' # <= transaction = FIAT (EUR in my case) purchase and deposit on account 
-        credited_asset = coin
-        credited_amount = change
         debited_asset = ''
         debited_amount = ''
-        fee_asset = ''
-        fee_amount = ''
-      elsif change < 0 # <= transaction = debit (should be FIAT - EUR in my case)
-        credited_asset = ''
-        credited_amount = ''
-        debited_asset = coin
-        debited_amount = change
-        fee_asset = ''
-        fee_amount = ''
-      elsif change > 0 && coin != 'EUR' # <= transaction = credit (crypto)
-        credited_asset = coin
-        credited_amount = change
-        debited_asset = ''
-        debited_amount = ''
-        fee_asset = ''
-        fee_amount = ''
+        fee_asset = coin
+        fee_amount = change
       end
 
-    when 'Fee' # OPERATION TYPE = "FEES"
-      credited_asset = ''
-      credited_amount = ''
-      debited_asset = ''
-      debited_amount = ''
-      fee_asset = coin
-      fee_amount = change
+
+      # Create an array for the modified transaction
+      modified_transaction = [platform, timestamp, account, operation, credited_asset, credited_amount, debited_asset, debited_amount, fee_asset, fee_amount, remark]
+
+      # Write the modified transaction to the new CSV file
+      csv << modified_transaction
+
+      puts "change.class = " # SHOULD BE A COMMENT
+      puts change.class # SHOULD BE A COMMENT
+      puts "- - - - -" # SHOULD BE A COMMENT
+
     end
-
-
-    # Create an array for the modified transaction
-    modified_transaction = [platform, timestamp, account, operation, credited_asset, credited_amount, debited_asset, debited_amount, fee_asset, fee_amount, remark]
-
-    # Write the modified transaction to the new CSV file
-    csv << modified_transaction
-
-    puts "change.class = " # SHOULD BE A COMMENT
-    puts change.class # SHOULD BE A COMMENT
-    puts "- - - - -" # SHOULD BE A COMMENT
-
   end
 end
 
@@ -166,3 +173,5 @@ STEP 3 => Iterate through each transaction to find related transactions and comp
 
 
 =end
+
+
